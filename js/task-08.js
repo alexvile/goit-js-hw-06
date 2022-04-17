90e099f386b986497f9512313372ba6640cfbdd8
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-04-17 21:43:26 +0300

    решает д/з №8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (formElements.email.value === '' || formElements.password.value === '') {
+        alert('Одно из полей ввода не заполнено !');
+    } else {
+        const formData = {
+        mail,
+        password,
+        };
+        
+        console.log(formData);
+        formEl.reset();
+        
+    }
+}
+
+
 
