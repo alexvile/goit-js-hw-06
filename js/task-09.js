2c7dd38a4de3a36e8b3fa7973e429b68979e34fa
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-04-24 12:45:37 +0300

    решает задачу №9 другим путем

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,23 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// };
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
 
 const magicBtn = document.querySelector('.change-color');
 const color = document.querySelector('.color');
 
  
-function getRandomHexColor() {
-  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function paintBody() {
+  const randomColor = getRandomHexColor();
   color.textContent = randomColor;
   document.body.style.backgroundColor = randomColor;
   
 };
 
+magicBtn.addEventListener('click', paintBody);
 
-magicBtn.addEventListener('click', getRandomHexColor);
+
+
+
+
+
